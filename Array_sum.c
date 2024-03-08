0a0edb680b8c5a62ab00767fbfa0255d57c99bfa
RaghavendraSadare <144417630+RaghavendraSadare@users.noreply.github.com>
2024-03-08 09:59:00 +0530

    Add files via upload

---
+++ Array_sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int numbers[] = {1, 2, 3, 4, 5};
+    int sum = 0;
+    for (int i = 0; i < 5; ++i)
+        sum += numbers[i];
+    printf("Sum: %d\n", sum);
+    return 0;
+}
+
